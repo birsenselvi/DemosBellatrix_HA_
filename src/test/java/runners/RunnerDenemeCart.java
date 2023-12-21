61f4f04daf4f58c2d97d3dcc7df4d3b8ec9bcece
BirsenSelviCoskun <birsenselvi88@gmail.com>
2023-12-21 12:09:49 +0100

    DemobellatrixHA

--- src/test/java/runners/RunnerDenemeCart.java
+++ src/test/java/runners/RunnerDenemeCart.java
@@ -6,9 +6,8 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = {"src/test/resources/FeatureDeneme.feature"},
         glue = {"stepdefs"},
-        plugin = {"pretty","html:target/cucumber/cucumber.html",
+        plugin = {"pretty",
                 "target/cucumber/cucumber.json",
-                "target/cucumber/cucumber.xml",
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
                 "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
         }
